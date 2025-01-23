2f529ca8d040ec68c99eab11df29d2affc6739c8
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-01-23 09:23:53 +0900

    add-profile

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -8,21 +8,34 @@ class BoardsController < ApplicationController
   end
 
   def create
-    # @board = current_user.boards.build(params[:board])
-    # @board.user = current_user
-    # if @board.save
-    #   redirect_to boards_path
-    # else
-    #   render :new
-    # end
+  end
+
+  def update
+    @board = current_user.boards.find(params[:id])
+    if @board.update(board_params)
+      redirect_to board_path(@board), notice: '更新できました'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
   end
 
   def show
     @board = Board.find(params[:id])
   end
 
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def destroy
+    @board = current_user.boards.find(params[:id])
+    @board.destroy
+      redirect_to boards_path, notice: '削除できました'
+  end
+
   private
   def board_params
-    params.require(:board).permit(:name, :description)
+    params.require(:board).permit(:title, :content, :deadline )
   end
 end
