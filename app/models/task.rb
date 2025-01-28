8ddd86d1eac390a24ae77df3dfdfe806cc6b936d
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-01-28 09:35:01 +0900

    add annotate

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  deadline   :date
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  board_id   :bigint
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_tasks_on_board_id  (board_id)
+#  index_tasks_on_user_id   (user_id)
+#
 class Task < ApplicationRecord
   belongs_to :board
   belongs_to :user
