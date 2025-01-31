ff9da7ef9977340d6cb727083d45f621d6ea3b25
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-01-31 13:28:51 +0900

    create comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,21 @@
 class CommentsController < ApplicationController
   def new
     @comment = Comment.new
-    binding.pry
   end
+
+  def create
+    board = Board.find(params[:board_id])
+    task = Task.find(params[:task_id])
+    @comment = task.comments.build(comment_params)
+    if @comment.save
+      binding.pry
+      redirect_to board_task_path(board,@comment), notice: '作成できました'
+    else
+      flash.now[:error] = '作成できませんでした'
+      render :new
+    end
+  end
+
 
   def destroy
     @comment = Comment.find(params[:id])
@@ -12,6 +25,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id)
+    params.require(:comment).permit(:content)
   end
 end
