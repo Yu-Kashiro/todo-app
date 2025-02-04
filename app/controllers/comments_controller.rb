5aa3156aa4d7a5feed161753f0b2f782b43897a1
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-02-04 11:25:33 +0900

    fix comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,15 +7,14 @@ class CommentsController < ApplicationController
     board = Board.find(params[:board_id])
     task = Task.find(params[:task_id])
     @comment = task.comments.build(comment_params)
+    @comment.user_id = current_user.id # Assuming you have a current_user method
     if @comment.save
-      binding.pry
-      redirect_to board_task_path(board,@comment), notice: '作成できました'
+      redirect_to board_task_path(board,task), notice: '作成できました'
     else
       flash.now[:error] = '作成できませんでした'
       render :new
     end
   end
-
 
   def destroy
     @comment = Comment.find(params[:id])
@@ -25,6 +24,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content, :task_id, :user_id)
   end
 end
